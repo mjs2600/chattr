6f10f2bdff76c5412c5308a7fd6a78a071cd7b67
Michael Simpson <mjs2600@gmail.com>
2016-01-01 11:42:43 -0500

    Bump versions

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Chattr.Mixfile do
 
   def project do
     [app: :chattr,
-     version: "0.0.1",
-     elixir: "~> 1.0",
+     version: "0.1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
